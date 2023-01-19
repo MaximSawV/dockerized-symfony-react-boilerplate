39db22f801beb09ef12d6c62e8a4cf562e4fca39
Maxim Saweljew <m.saweljew@venabo.de>
2023-01-19 10:06:21 +0100

    refactor

--- app/src/Domain/Kanban/KanbanBoard/KanbanColumn/components/KanbanColumn.tsx
+++ app/src/Domain/Kanban/KanbanBoard/KanbanColumn/components/KanbanColumn.tsx
@@ -1,95 +1,55 @@
 import React from 'react';
-import {Draggable, Droppable} from 'react-beautiful-dnd';
 import {KanbanCardItem} from "../../lib/source";
 import {css} from "@emotion/css";
-import {Avatar, Card} from "antd";
-import Meta from "antd/lib/card/Meta";
+import KanbanCard from "../KanbanCard/components/KanbanCard";
+import {useDrop} from "react-dnd";
+import {ItemTypes} from "../../components/KanbanBoard";
 
 interface KanbanColumnProps {
     column: { items: KanbanCardItem[], name: string };
-    columnId: string;
-
+    id: string
 }
 
 export default function KanbanColumn(props: KanbanColumnProps) {
 
-    const {column, columnId} = props;
+    const {column, id} = props;
+
+    const [collectedProps, drop] = useDrop(() => ({
+        accept: ItemTypes.CARD
+    }))
 
     return (
         <div
+            key={id}
             className={css`
               display: flex;
               flex-direction: column;
               align-items: center;
             `}
-            key={columnId}
         >
             <p>{column.name}</p>
             <div style={{margin: 16}}>
-                <Droppable droppableId={columnId} key={columnId}>
-                    {(provided) => {
-                        return (
-                            <div
-                                {...provided.droppableProps}
-                                ref={provided.innerRef}
-                                className={css`
-                                  background: #c7d9e1;
-                                  padding: 1em;
-                                  width: 16em;
-                                  min-height: 60em;
-                                `}
-                            >
-                                {column.items.map((item, index) => {
-                                    return (
-                                        <Draggable
-                                            key={item.id}
-                                            draggableId={item.id}
-                                            index={index}
-                                        >
-                                            {(provided) => {
-                                                return (
-                                                    <div
-                                                        ref={provided.innerRef}
-                                                        {...provided.draggableProps}
-                                                        {...provided.dragHandleProps}
-                                                        style={{
-                                                            userSelect: "none",
-                                                            padding: 16,
-                                                            margin: "0 0 8px 0",
-                                                            ...provided.draggableProps.style
-                                                        }}
-                                                    >
-                                                        <Card
-                                                            size={"small"} key={item.id}
-                                                            actions={item.participants.map((participant) => {
-                                                                return (
-                                                                    <Avatar
-                                                                        className={css`background-color: ${participant.color}`}>
-                                                                        {participant.name}
-                                                                    </Avatar>
-                                                                );
-                                                            })}
-                                                        >
-                                                            <Meta
-                                                                avatar={
-                                                                <Avatar className={css`background: ${item.createdBy.color}`}>
-                                                                    {item.createdBy.name}
-                                                                </Avatar> }
-                                                                title={item.title}
-                                                            />
-                                                            <p>{item.description}</p>
-                                                        </Card>
-                                                    </div>
-                                                );
-                                            }}
-                                        </Draggable>
-                                    );
-                                })}
-                                {provided.placeholder}
-                            </div>
-                        );
-                    }}
-                </Droppable>
+                <div
+                    className={css`
+                      background: #c7d9e1;
+                      padding: 1em;
+                      width: 16em;
+                      min-height: 60em;
+                    `}
+                >
+                    <div
+                        ref={drop}
+                        style={{
+                            userSelect: "none",
+                        }}
+                    >
+                        {column.items.map((item, index) => {
+                            return (
+                                <KanbanCard item={item} key={item.id}/>
+                            );
+                        })}
+                    </div>
+                </div>
             </div>
         </div>
     );
--- app/src/Domain/Kanban/KanbanBoard/components/KanbanBoard.tsx
+++ app/src/Domain/Kanban/KanbanBoard/components/KanbanBoard.tsx
@@ -1,9 +1,13 @@
 import React, {useState} from "react";
-import {DragDropContext, DropResult} from "react-beautiful-dnd";
 import {cards, columnsFromBackend, KanbanColumnItem} from "../lib/source";
 import {css} from "@emotion/css";
 import KanbanColumn from "../KanbanColumn/components/KanbanColumn";
+import {DndProvider} from "react-dnd";
+import {HTML5Backend} from "react-dnd-html5-backend";
 
+export const ItemTypes = {
+    CARD: 'card'
+}
 export default function KanbanBoard() {
 
     const [columns, setColumns] = useState<KanbanColumnItem>(columnsFromBackend);
@@ -15,42 +19,6 @@ export default function KanbanBoard() {
         })
         setLoaded(true);
     }
-    const onDragEnd = (result: DropResult) => {
-        if (!result.destination) return;
-        const {source, destination} = result;
-
-        if (source.droppableId !== destination?.droppableId) {
-            const sourceColumn = columns[source.droppableId];
-            const destColumn = columns[destination?.droppableId];
-            const sourceItems = [...sourceColumn.items];
-            const destItems = [...destColumn.items];
-            const [removed] = sourceItems.splice(source.index, 1);
-            destItems.splice(destination?.index, 0, removed);
-            setColumns({
-                ...columns,
-                [source.droppableId]: {
-                    ...sourceColumn,
-                    items: sourceItems
-                },
-                [destination?.droppableId]: {
-                    ...destColumn,
-                    items: destItems
-                }
-            });
-        } else {
-            const column = columns[source.droppableId];
-            const copiedItems = [...column.items];
-            const [removed] = copiedItems.splice(source.index, 1);
-            copiedItems.splice(destination?.index, 0, removed);
-            setColumns({
-                ...columns,
-                [source.droppableId]: {
-                    ...column,
-                    items: copiedItems
-                }
-            });
-        }
-    }
 
     return (
         <div
@@ -61,13 +29,13 @@ export default function KanbanBoard() {
               height: 100%;
             `}
         >
-            <DragDropContext onDragEnd={result => onDragEnd(result)}>
+            <DndProvider backend={HTML5Backend}>
                 {Object.entries(columns).map(([columnId, column]) => {
                     return (
-                        <KanbanColumn key={columnId} columnId={columnId} column={column} />
+                        <KanbanColumn key={columnId} id={columnId} column={column} />
                     );
                 })}
-            </DragDropContext>
+            </DndProvider>
         </div>
     );
 }
